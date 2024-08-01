4483b191b8c02daa17ed87572d74c05223cd87df
Zhang Jing <mrzleo@foxmail.com>
2024-08-01 16:52:23 +0800

    fix(rbtree): fix format error

--- utils/tools.h
+++ utils/tools.h
@@ -1,8 +1,10 @@
 #ifndef LIBCOVE_UTILS_TOOLS_H_
 #define LIBCOVE_UTILS_TOOLS_H_
 
-#define container_of(ptr, type, member) ({                      \
-        const typeof( ((type *)0)->member ) *__mptr = (ptr);    \
-        (type *)( (char *)__mptr - offsetof(type, member) );})
+#define container_of(ptr, type, member)                      \
+    ({                                                       \
+        const typeof(((type *) 0)->member) *__mptr = (ptr);  \
+        (type *) ((char *) __mptr - offsetof(type, member)); \
+    })
 
 #endif
